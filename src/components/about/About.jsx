4f330615b70234ccabed269f25725aa68fde0e7b
RUSSEN BISSEMO <russenbissemo@gmail.com>
2022-11-06 12:55:39 +0100

    progression portofolio

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import "./About.css"
-import ME from '../../assets/me-about.jpg'
+import ME from '../../assets/rosbain3.jpg'
 import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
@@ -12,8 +14,8 @@ const About = () => {
 
       <div className=' container about-container'>
         <div className='about-me'>
-          <div className ="about-me-image">
-            <img src={ME} alt="About Image"/>
+          <div className ="about-me-im">
+            <img src={ME} alt="About Portrait"/>
           </div>
         </div>
         <div className='about-content'> 
@@ -26,18 +28,21 @@ const About = () => {
             </article>
 
             <article className='about-card'>
-              <FaAward className='About-icon'/>
-              <h5> Experience</h5>
-              <small> 1 Year Experience</small>
+              <FiUsers className='About-icon'/>
+              <h5>clients</h5>
+              <small>3</small>
             </article>
 
             <article className='about-card'>
-              <FaAward className='About-icon'/>
-              <h5> Experience</h5>
-              <small> 1 Year Experience</small>
+              <VscFolderLibrary className='About-icon'/>
+              <h5>Projects</h5>
+              <small>3 Completed</small>
             </article>
           </div>
-
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda laudantium ipsum ullam dolorum vero quia provident beatae illo, laborum sint
+          </p>
+          <a href = "#contact" className='btn btn-primary'>Let's Talk</a>
         </div>
 
       </div>
